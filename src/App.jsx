58b0a73dc41d37429ce055857b94e79300a92026
itsbn0110 <itsbn610@gmail.com>
2024-10-22 00:03:48 +0700

    add_theme_mui

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { Button } from '@mui/material';
 import './App.css';
 import { AccessAlarm } from '@mui/icons-material';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-
+import Typography from '@mui/material/Typography';
 function App() {
   return (
     <>
       <h1>Bảo Ngô</h1>
+      <Typography variant="h1" color="text.secondary">
+        Test Typography
+      </Typography>
       <Button variant="contained">Contained</Button>
       <Button variant="text">Text</Button>
       <Button variant="outlined">Outlined</Button>
