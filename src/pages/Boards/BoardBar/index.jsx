e703592cc6964cd15667bd478af1b912da67675a
itsbn0110 <itsbn610@gmail.com>
2024-10-24 23:27:09 +0700

    build-board-content-ui

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -34,16 +34,19 @@ function BoardBar() {
         alignItems: 'center',
         justifyContent: 'space-between',
         gap: 2,
-        overflow: 'auto',
         borderBottom: '1px solid white',
         paddingX: 2,
-        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2')
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        overflow: 'auto',
+        '&::-webkit-scrollbar-track': {
+          m: 2
+        }
       }}
     >
       <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: 2 }}>
         <Chip icon={<DashboardIcon />} label="BaoNgoDev Board" sx={CHIP_STYLES} clickable />
         <Chip icon={<VpnLockIcon />} label="Public/Private Workspace" sx={CHIP_STYLES} clickable />
-        <Chip icon={<AddToDriveIcon />} label="BaoNgoDev Board" sx={CHIP_STYLES} clickable />
+        <Chip icon={<AddToDriveIcon />} label="Add to Google Drive" sx={CHIP_STYLES} clickable />
         <Chip icon={<BoltIcon />} label="Automation" sx={CHIP_STYLES} clickable />
         <Chip icon={<FilterListIcon />} label="Filters" sx={CHIP_STYLES} clickable />
       </Box>
@@ -63,7 +66,18 @@ function BoardBar() {
         <AvatarGroup
           max={7}
           total={10}
-          sx={{ gap: '10px', '& .MuiAvatar-root': { width: 36, height: 36, fontSize: 16, border: 'none' } }}
+          sx={{
+            gap: '10px',
+            '& .MuiAvatar-root': {
+              width: 36,
+              height: 36,
+              fontSize: 16,
+              border: 'none',
+              cursor: 'pointer',
+              color: 'white',
+              '&:first-of-type': { bgColor: '#a4b0be' }
+            }
+          }}
         >
           <Tooltip title="HoaXu">
             <Avatar
@@ -75,34 +89,19 @@ function BoardBar() {
             <Avatar alt="BaoNgo" src="https://avatars.githubusercontent.com/u/181921020?v=4" />
           </Tooltip>
           <Tooltip title="Rose">
-            <Avatar
-              alt="Rose"
-              src="https://i.pinimg.com/enabled_hi/564x/f6/cb/78/f6cb78a2affe7954a5c699313a382278.jpg"
-            />
+            <Avatar alt="Rose" src="https://i.pinimg.com/enabled_hi/564x/f6/cb/78/f6cb78a2affe7954a5c699313a382278.jpg" />
           </Tooltip>
           <Tooltip title="Zhāng Ruò Nán / 章若楠">
-            <Avatar
-              alt="Zhāng Ruò Nán / 章若楠"
-              src="https://i.pinimg.com/564x/a7/67/47/a767478c979c6274c1de6dab0066606d.jpg"
-            />
+            <Avatar alt="Zhāng Ruò Nán / 章若楠" src="https://i.pinimg.com/564x/a7/67/47/a767478c979c6274c1de6dab0066606d.jpg" />
           </Tooltip>
           <Tooltip title="Zhāng Ruò Nán / 章若楠">
-            <Avatar
-              alt="Zhāng Ruò Nán / 章若楠"
-              src="https://i.pinimg.com/736x/65/fe/29/65fe29d7c661a92f5c716b7f234444cd.jpg"
-            />
+            <Avatar alt="Zhāng Ruò Nán / 章若楠" src="https://i.pinimg.com/736x/65/fe/29/65fe29d7c661a92f5c716b7f234444cd.jpg" />
           </Tooltip>
           <Tooltip title="Zhāng Ruò Nán / 章若楠">
-            <Avatar
-              alt="Zhāng Ruò Nán / 章若楠"
-              src="https://i.pinimg.com/736x/9c/b2/11/9cb211670c950870669fd722479a1e35.jpg"
-            />
+            <Avatar alt="Zhāng Ruò Nán / 章若楠" src="https://i.pinimg.com/736x/9c/b2/11/9cb211670c950870669fd722479a1e35.jpg" />
           </Tooltip>
           <Tooltip title="Zhāng Ruò Nán / 章若楠">
-            <Avatar
-              alt="Zhāng Ruò Nán / 章若楠"
-              src="https://i.pinimg.com/564x/73/88/64/73886422a42cb76fa30822d632ef4b15.jpg"
-            />
+            <Avatar alt="Zhāng Ruò Nán / 章若楠" src="https://i.pinimg.com/564x/73/88/64/73886422a42cb76fa30822d632ef4b15.jpg" />
           </Tooltip>
           <Tooltip title="HoaXu">
             <Avatar
@@ -114,34 +113,19 @@ function BoardBar() {
             <Avatar alt="BaoNgo" src="https://avatars.githubusercontent.com/u/181921020?v=4" />
           </Tooltip>
           <Tooltip title="Rose">
-            <Avatar
-              alt="Rose"
-              src="https://i.pinimg.com/enabled_hi/564x/f6/cb/78/f6cb78a2affe7954a5c699313a382278.jpg"
-            />
+            <Avatar alt="Rose" src="https://i.pinimg.com/enabled_hi/564x/f6/cb/78/f6cb78a2affe7954a5c699313a382278.jpg" />
           </Tooltip>
           <Tooltip title="Zhāng Ruò Nán / 章若楠">
-            <Avatar
-              alt="Zhāng Ruò Nán / 章若楠"
-              src="https://i.pinimg.com/564x/a7/67/47/a767478c979c6274c1de6dab0066606d.jpg"
-            />
+            <Avatar alt="Zhāng Ruò Nán / 章若楠" src="https://i.pinimg.com/564x/a7/67/47/a767478c979c6274c1de6dab0066606d.jpg" />
           </Tooltip>
           <Tooltip title="Zhāng Ruò Nán / 章若楠">
-            <Avatar
-              alt="Zhāng Ruò Nán / 章若楠"
-              src="https://i.pinimg.com/736x/65/fe/29/65fe29d7c661a92f5c716b7f234444cd.jpg"
-            />
+            <Avatar alt="Zhāng Ruò Nán / 章若楠" src="https://i.pinimg.com/736x/65/fe/29/65fe29d7c661a92f5c716b7f234444cd.jpg" />
           </Tooltip>
           <Tooltip title="Zhāng Ruò Nán / 章若楠">
-            <Avatar
-              alt="Zhāng Ruò Nán / 章若楠"
-              src="https://i.pinimg.com/736x/9c/b2/11/9cb211670c950870669fd722479a1e35.jpg"
-            />
+            <Avatar alt="Zhāng Ruò Nán / 章若楠" src="https://i.pinimg.com/736x/9c/b2/11/9cb211670c950870669fd722479a1e35.jpg" />
           </Tooltip>
           <Tooltip title="Zhāng Ruò Nán / 章若楠">
-            <Avatar
-              alt="Zhāng Ruò Nán / 章若楠"
-              src="https://i.pinimg.com/564x/73/88/64/73886422a42cb76fa30822d632ef4b15.jpg"
-            />
+            <Avatar alt="Zhāng Ruò Nán / 章若楠" src="https://i.pinimg.com/564x/73/88/64/73886422a42cb76fa30822d632ef4b15.jpg" />
           </Tooltip>
         </AvatarGroup>
       </Box>
