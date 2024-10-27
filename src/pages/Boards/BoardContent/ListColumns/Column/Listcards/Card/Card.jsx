15a7ec934000e994bfd8273462e321e8baa410b6
itsbn0110 <itsbn610@gmail.com>
2024-10-28 01:28:38 +0700

    drag_drop_overlay/animation_drop

--- src/pages/Boards/BoardContent/ListColumns/Column/Listcards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Listcards/Card/Card.jsx
@@ -7,12 +7,36 @@ import CardMedia from '@mui/material/CardMedia';
 import GroupIcon from '@mui/icons-material/Group';
 import CommentIcon from '@mui/icons-material/Comment';
 import AttachmentIcon from '@mui/icons-material/Attachment';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 function Card({ card }) {
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+    id: card._id,
+    data: { ...card }
+  });
+  const dndKitCardStyles = {
+    // For sensor default has PointerSensor
+    // touchAction: 'none',
+    // if use CSS.transform will be bug like 'stretch'
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : undefined
+  };
   const shouldShowCardAction = () => {
     return card?.memberIds?.length || card?.comments?.length || card?.attachments?.length;
   };
   return (
-    <MuiCard sx={{ cursor: 'pointer', boxShadow: '0 1px 1px rgba(0,0,0,0.2)', overflow: 'unset' }}>
+    <MuiCard
+      sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0,0,0,0.2)',
+        overflow: 'unset'
+      }}
+      style={dndKitCardStyles}
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+    >
       {card?.cover && (
         <CardMedia
           sx={{
--- src/pages/Boards/BoardContent/ListColumns/Column/Listcards/Listcards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Listcards/Listcards.jsx
@@ -1,34 +1,38 @@
 import Box from '@mui/material/Box';
 import Card from './Card/Card';
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
+
 function ListCards({ cards }) {
   return (
-    <Box
-      sx={{
-        m: '0 5px',
-        p: '0 5px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 1,
-        overflowX: 'hidden',
-        overflowY: 'auto',
-        maxHeight: (theme) => `calc(
+    <SortableContext items={cards?.map((c) => c._id)} strategy={verticalListSortingStrategy}>
+      <Box
+        sx={{
+          m: '0 5px',
+          p: '0 5px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          overflowX: 'hidden',
+          overflowY: 'auto',
+          maxHeight: (theme) => `calc(
         ${theme.trello.boardContentHeight} - 
         ${theme.spacing(5)} - 
         ${theme.trello.columnHeaderHeight} - 
         ${theme.trello.columnFooterHeight})`,
-        '::-webkit-scrollbar-thumb': {
-          backgroundColor: '#ced0da',
-          borderRadius: '4px'
-        },
-        '::-webkit-scrollbar-thumb:hover': {
-          backgroundColor: '#bfc2cf'
-        }
-      }}
-    >
-      {cards?.map((card) => {
-        return <Card key={card._id} card={card} />;
-      })}
-    </Box>
+          '::-webkit-scrollbar-thumb': {
+            backgroundColor: '#ced0da',
+            borderRadius: '4px'
+          },
+          '::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#bfc2cf'
+          }
+        }}
+      >
+        {cards?.map((card) => {
+          return <Card key={card._id} card={card} />;
+        })}
+      </Box>
+    </SortableContext>
   );
 }
 
