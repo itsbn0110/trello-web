afbf20ee00ba37af8b27a9d1ac050c2bbc93cc2e
itsbn0110 <itsbn610@gmail.com>
2024-11-07 00:41:38 +0700

    Fix Build Mode dev and producion

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,11 @@
-export const API_ROOT = 'http://localhost:8386';
+let apiRoot = '';
+// import.meta.env là của Vite
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8386';
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-wcmn.onrender.com';
+}
+
+export const API_ROOT = apiRoot;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
+  // cho phép Vite sử dụng được process.env, không thì phải dùng import.meta.env
+  define: {
+    'process.env': process.env
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '~', replacement: '/src' }]
