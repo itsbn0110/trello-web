a2ca3fb35af5b832c6a66bb572ec23e23daed03c
itsbn0110 <itsbn610@gmail.com>
2024-12-05 09:25:13 +0700

    UploadFileToCloudinary/BoardLis

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '~/redux/user/userSlice';
 
 import Settings from '~/pages/Settings/Settings';
+
+import Boards from './pages/Boards';
 /**
  * Giải pháp Clean Code trong việc xác định các route nào cần đăng nhập tài khoản mới cho truy cập
  * Sử dụng <Outlet/> của react-router-dom để hiển thị các Child Route (xem cách sử dụng trong App() bên dưới)
@@ -42,6 +44,8 @@ function App() {
         {/* <Outlet /> của react-router-dom sẽ chạy vào các child route trong này */}
         {/* Board Details */}
         <Route path="/boards/:boardId" element={<Board />} />
+        <Route path="/boards" element={<Boards />} />
+
         {/* User Settings */}
         <Route path="/settings/account" element={<Settings />} />
         <Route path="/settings/security" element={<Settings />} />
