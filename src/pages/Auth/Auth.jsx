8e27414796d647c63d6318758d425e439bf8ac91
itsbn0110 <itsbn610@gmail.com>
2024-12-13 00:12:03 +0700

    change-bg-login/register

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -4,6 +4,7 @@ import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '~/redux/user/userSlice';
+import bgImage from '~/assets/auth/login-register-bg.jpg'
 function Auth() {
   const location = useLocation();
   // console.log(location)
@@ -23,7 +24,7 @@ function Auth() {
         minHeight: '100vh',
         alignItems: 'center',
         justifyContent: 'flex-start',
-        background: 'url("src/assets/auth/login-register-bg.jpg")',
+        background: `url(${bgImage})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
